5c17be1c96a25b7bbc38e2b08e700816e9cbe424
Flavien Schriever <flavien@schriever.dev>
2023-01-27 13:11:03 +0100

    feat: add OpeningController

---
+++ src/main/java/dev/schriever/apps/shakmat/web/OpeningController.java
@@ -0,0 +1,53 @@
+package dev.schriever.apps.shakmat.web;
+
+import dev.schriever.apps.shakmat.dto.GetOpeningDto;
+import dev.schriever.apps.shakmat.entity.Opening;
+import dev.schriever.apps.shakmat.exception.OpeningNotFoundException;
+import dev.schriever.apps.shakmat.mapper.OpeningMapper;
+import dev.schriever.apps.shakmat.service.OpeningService;
+import jakarta.websocket.server.PathParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/opening")
+public class OpeningController {
+
+    private final OpeningService openingService;
+    private final OpeningMapper openingMapper;
+
+    public OpeningController(OpeningService openingService, OpeningMapper openingMapper) {
+        this.openingService = openingService;
+        this.openingMapper = openingMapper;
+    }
+
+    @GetMapping
+    public List<GetOpeningDto> getOpenings(@PathParam("moves") String moves) {
+        if (moves != null) {
+            return openingService.listOpeningByMovesStartsWith(moves)
+                    .stream()
+                    .map(openingMapper::toDto)
+                    .collect(Collectors.toList());
+        }
+        return openingService.listOpening()
+                .stream()
+                .map(openingMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("{id}")
+    public GetOpeningDto getOpening(@PathVariable("id") Long id) {
+        try {
+            return openingMapper.toDto(openingService.findOpeningById(id));
+        } catch (OpeningNotFoundException exception) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, exception.getMessage());
+        }
+    }
+}
