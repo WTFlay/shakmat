5c497a3eaf16ba9b79e765a2e7cffecd4e8859c8
Flavien Schriever <flavien@schriever.dev>
2023-01-27 11:07:31 +0100

    feat: init commit

---
+++ src/main/java/dev/schriever/apps/shakmat/ShakmatApplication.java
@@ -0,0 +1,13 @@
+package dev.schriever.apps.shakmat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShakmatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShakmatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/schriever/apps/shakmat/ShakmatApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.schriever.apps.shakmat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShakmatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
